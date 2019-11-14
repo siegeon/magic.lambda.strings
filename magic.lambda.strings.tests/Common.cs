6ead6d02923cb15fa6088dacc2a3ffb70defca68
Thomas Hansen <thomas@gaiasoul.com>
2019-11-14 16:22:32 +0200

    .Net Core 3 fixes

--- magic.lambda.strings.tests/Common.cs
+++ magic.lambda.strings.tests/Common.cs
@@ -44,6 +44,7 @@ namespace magic.lambda.strings.tests
         {
             var type = typeof(T);
             var result = AppDomain.CurrentDomain.GetAssemblies()
+                .Where(x => !x.IsDynamic && !x.FullName.StartsWith("Microsoft"))
                 .SelectMany(s => s.GetTypes())
                 .Where(p => type.IsAssignableFrom(p) && !p.IsInterface && !p.IsAbstract);
 
