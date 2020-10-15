c7c1e8244c12eab38f01aa3acafb91ac3e654387
Thomas Hansen <thomas@gaiasoul.com>
2020-10-15 15:54:36 +0300

    Adding [strings.url-encode] slot for URL encoding strings

---
+++ magic.lambda.strings/UrlEncode.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Magic, Copyright(c) Thomas Hansen 2019 - 2020, thomas@servergardens.com, all rights reserved.
+ * See the enclosed LICENSE file for details.
+ */
+
+using System.Net;
+using magic.node;
+using magic.node.extensions;
+using magic.signals.contracts;
+
+namespace magic.lambda.strings
+{
+    /// <summary>
+    /// [strings.url-encode] slot that URL encodes the specified string.
+    /// </summary>
+    [Slot(Name = "strings.url-encode")]
+    public class UrlEncode : ISlot
+    {
+        /// <summary>
+        /// Implementation of slot.
+        /// </summary>
+        /// <param name="signaler">Signaler used to raise signal.</param>
+        /// <param name="input">Arguments to your slot.</param>
+        public void Signal(ISignaler signaler, Node input)
+        {
+            input.Value = WebUtility.UrlEncode(input.GetEx<string>());
+        }
+    }
+}
